28799721bdf6987e2209c955937f8299fafcf2af
Chip Castle <chip@chipcastle.com>
2010-09-29 09:25:52 -0500

    Initial commit

---
+++ Classes/phonegap_iphone_invoicethatAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  phonegap_iphone_invoicethatAppDelegate.h
+//  phonegap-iphone-invoicethat
+//
+//  Created by Chip Castle on 9/19/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface phonegap_iphone_invoicethatAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ www/iphone.js
@@ -0,0 +1,29 @@
+if (window.innerWidth && window.innerWidth <= 480) {
+	$(document).ready(function(){
+		$('#header ul').addClass('hide');
+		$('#header').append('<div class="leftButton" onclick="toggleMenu()">Menu</div>');
+	});
+	function toggleMenu() { 
+		$('#header ul').toggleClass('hide');
+		$('#header .leftButton').toggleClass('pressed');
+	}
+}
+
+$(document).ready(function(){
+	loadPage();
+});
+function loadPage(url) {
+    $('body').append('<div id="progress">Loading...</div>');
+    if (url == undefined) {
+        $('#container').load('index.html #header ul', hijackLinks);
+    } else {
+        $('#container').load(url + ' #content', hijackLinks);
+    }
+}
+function hijackLinks() {
+    $('#container a').click(function(e){
+		e.preventDefault();
+		loadPage(e.target.href);
+	});
+    $('#progress').remove();
+}
