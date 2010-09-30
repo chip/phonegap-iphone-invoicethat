0c9599359f76bb7134083748dafaeb89c584bd19
Chip Castle <chip@chipcastle.com>
2010-09-30 11:05:09 -0500

    Reworking index page to include on 2 buttons for 'Are you a member?': Yes, No.

--- www/mobile.js
+++ www/mobile.js
@@ -1,50 +1,27 @@
-if (window.innerWidth && window.innerWidth <= 480) {
-	$(document).ready(function(){
-		$('#header ul').addClass('hide');
-		$('#header').append('<div class="leftButton" onclick="toggleMenu()">Menu</div>');
-	});
-	function toggleMenu() { 
-		$('#header ul').toggleClass('hide');
-		$('#header .leftButton').toggleClass('pressed');
-	}
-}
+/*jslint white: false, onevar: true, browser: true, devel: true, undef: false, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, strict: false, newcap: true, immed: true */
+/* See http://www.jslint.com/ */
 
-var hist = [];
-var startUrl = 'index.html';
-$(document).ready(function(){
-	//loadPage(startUrl);
-});
-function loadPage(url) {
-    $('body').append('<div id="progress">Loading...</div>');
-    scrollTo(0,0);
-    if (url == startUrl) {
-        var element = ' #header ul';
-    } else {
-        var element = ' #content';
-    }
-    $('#home').load(url + element, function() {
-		var title = $('h2').html() || 'invoicethat.com';
-		$('h1').html(title);
-		$('h2').remove();
-		$('.leftButton').remove();
-		hist.unshift({'url':url, 'title':title});
-		if (hist.length > 1) {
-			$('#header').append('<div class="leftButton">'+hist[1].title+'</div>');
-			$('#header .leftButton').click(function(){
-				var thisPage = hist.shift();
-				var previousPage = hist.shift();
-				loadPage(previousPage.url);
-			});
-		}
-		$('#container a').click(function(e){
-			var url = e.target.href;
-			alert('url = ' + url);
-			if (url.match(/invoicethat.com/)) {
-				e.preventDefault();
-				loadPage(url);
-			}
-		});
-		alert('removing #progress');
-		$('#progress').remove();
-	});
-}
+var jQT = new $.jQTouch({
+						//icon: 'apple-touch-icon.png',
+						//startupScreen: "apple-touch-startup.png",
+						statusBar: 'black-translucent',
+						cacheGetRequests: false,
+						preloadImages: [
+										'/jqtouch/themes/jqt/img/back_button.png',
+										'/jqtouch/themes/jqt/img/back_button_clicked.png',
+										'/jqtouch/themes/jqt/img/button_clicked.png',
+										'/jqtouch/themes/jqt/img/grayButton.png',
+										'/jqtouch/themes/jqt/img/whiteButton.png',
+										'/jqtouch/themes/jqt/img/loading.gif'
+										]
+						});
+
+var app = {	
+	load_home: function(url) {
+		$.get(url, function(data) {
+			  // alert('load_home url=' + url);
+			  $('.current#home').html(data);
+			  });
+		return false;
+	}
+};
