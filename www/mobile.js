c615191140475b77c27a8b8df493d25d70b6ca14
Chip Castle <chip@chipcastle.com>
2010-10-05 20:22:18 -0500

    Got progress div overlay to work, login page of Rails app is called, devel variable setting determines url to call for launching app.

--- www/mobile.js
+++ www/mobile.js
@@ -6,36 +6,57 @@ var jQT = new $.jQTouch({
 	statusBar: 'black-translucent',
 	cacheGetRequests: false,
 	preloadImages: [
-		'./jqtouch/themes/apple/img/chevron_circle.png',
-		'./jqtouch/themes/apple/img/back_button_clicked.png',
-		'./jqtouch/themes/apple/img/button_clicked.png'
+		'./jqtouch/themes/apple/img/actionButton.png',
+		'./jqtouch/themes/apple/img/backButton.png',
+		'./jqtouch/themes/apple/img/blueButton.png',
+		'./jqtouch/themes/apple/img/cancel.png',
+		'./jqtouch/themes/apple/img/chevron.png',
+		'./jqtouch/themes/apple/img/grayButton.png',
+		'./jqtouch/themes/apple/img/greenButton.png',
+		'./jqtouch/themes/apple/img/listArrowSel.png',
+		'./jqtouch/themes/apple/img/listGroup.png',
+		'./jqtouch/themes/apple/img/loading.gif',
+		'./jqtouch/themes/apple/img/on_off.png',
+		'./jqtouch/themes/apple/img/pinstripes.png',
+		'./jqtouch/themes/apple/img/redButton.png',
+		'./jqtouch/themes/apple/img/selection.png',
+		'./jqtouch/themes/apple/img/thumb.png',
+		'./jqtouch/themes/apple/img/toggle.png',
+		'./jqtouch/themes/apple/img/toggleOn.png',
+		'./jqtouch/themes/apple/img/toolbar.png',
+		'./jqtouch/themes/apple/img/toolButton.png',
+		'./jqtouch/themes/apple/img/whiteButton.png'
 	]
 });
 
-$(function() {
+var devel = true;
 
-	var app = {	
-		load_home: function(url) {
-			alert('load_home url=' + url);
-			$('body').append('<div id="progress">Loading...</div>');
-			alert('load_home progress');
+$(function() {
+	//
+	// Need to setup iphone, mobile and m subdomains locally and on slice!!!
+	//
+	var base_url = devel ? 'http://iphone.invoicethat.local' : 'http://iphone.invoicethat.com'
+	var iphone_app = {	
+		load_home: function(id) {
+			$('#progress').show();
+			var href = $(id).attr('href');
+			//alert('id=' + id + ' href=' + href);
+
+			var url = base_url + href;
+			//alert('load_home url=' + url);
 			$.get(url, function(data) {
-				alert('load_home get url=' + url);
+				//alert('load_home get url=' + url);
 				$('.current#home').html(data);
 			});
-			alert('load_home progress remove');
-			$('#progress').remove();
 			return false;
 		}
 	};
   
 	$("a#login").tap(function (e) {
-		alert('tap a#login');
-		return app.load_home("http://invoicethat.local/login");
+		return iphone_app.load_home("a#login");
 	});
 
 	$("a#signup").tap(function (e) {
-		alert('tap a#signup');
-		return app.load_home("http://invoicethat.local/signup");
+		return iphone_app.load_home("a#signup");
 	});
 });
