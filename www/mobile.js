e513961e58f5768f55d5f0ec1a1dc3bd1d24c925
Chip Castle <chip@chipcastle.com>
2010-10-21 20:50:07 -0500

    Cleaned up project to use relative url's against the local server.  Will test production server later.

--- www/mobile.js
+++ www/mobile.js
@@ -1,62 +1,41 @@
 /*jslint white: false, onevar: true, browser: true, devel: true, undef: false, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, strict: false, newcap: true, immed: true */
 /* See http://www.jslint.com/ */
 
+
 var jQT = new $.jQTouch({
-	icon: './images/apple-touch-icon.png',
-	statusBar: 'black-translucent',
 	cacheGetRequests: false,
 	preloadImages: [
-		'./jqtouch/themes/apple/img/actionButton.png',
-		'./jqtouch/themes/apple/img/backButton.png',
-		'./jqtouch/themes/apple/img/blueButton.png',
-		'./jqtouch/themes/apple/img/cancel.png',
-		'./jqtouch/themes/apple/img/chevron.png',
-		'./jqtouch/themes/apple/img/grayButton.png',
-		'./jqtouch/themes/apple/img/greenButton.png',
-		'./jqtouch/themes/apple/img/listArrowSel.png',
-		'./jqtouch/themes/apple/img/listGroup.png',
-		'./jqtouch/themes/apple/img/loading.gif',
-		'./jqtouch/themes/apple/img/on_off.png',
-		'./jqtouch/themes/apple/img/pinstripes.png',
-		'./jqtouch/themes/apple/img/redButton.png',
-		'./jqtouch/themes/apple/img/selection.png',
-		'./jqtouch/themes/apple/img/thumb.png',
-		'./jqtouch/themes/apple/img/toggle.png',
-		'./jqtouch/themes/apple/img/toggleOn.png',
-		'./jqtouch/themes/apple/img/toolbar.png',
-		'./jqtouch/themes/apple/img/toolButton.png',
 		'./jqtouch/themes/apple/img/whiteButton.png'
 	]
 });
 
+//alert('inside mobile.js after jQT');
 $(function() {
-	alert('inside mobile.js');
+	
+	// SET TO true BEFORE APP SUBMISSION
 	var devel = true;
-	var base_url = devel ? 'http://m.invoicethat.local' : 'https://m.invoicethat.com';
-	var iphone_app = {	
-		load_home: function(id) {
-			//$('#progress').show();
-			alert('load_home');
-			$('body').append('<div id="progress">Loading.........</div>');
-			var href = $(id).attr('href');
-			alert('id=' + id + ' href=' + href);
-
-			var url = base_url + href;
-			//alert('load_home url=' + url);
-			$.get(url, function(data) {
-				//alert('load_home get url=' + url);
-				$('.current#home').html(data);
-			});
-			$('#progress').remove();
-			return false;
-		}
+	// BUY SSL CERT FOR m.invoicethat.com
+	var base_url = devel ? 'http://m.invoicethat.local' : 'http://m.invoicethat.com';
+	
+	$.LoadUrl = function(path) {
+		//alert('append progress to body.  path=' + path);
+		$('body').append('<div id="progress">Loading...</div>');
+		var $url = base_url + $(path).attr("href");
+		$.get($url, function(data) {
+			  //alert('phonegap: LoadUrl url=' + $url);
+			  $('.current#home').html(data);
+		});
+		$('#progress').remove();
+		return false;
 	};
-  
-	$("a#login").tap(function (e) {
-		return iphone_app.load_home("a#login");
+	
+
+	$("a#login").click(function (e) {
+		//navigator.notification.alert("alert", "title", "OK");
+		return $.LoadUrl("a#login");
 	});
 
-	$("a#pricing").tap(function (e) {
-		return iphone_app.load_home("a#pricing");
+	$("a#pricing").click(function (e) {
+		return $.LoadUrl("a#pricing");
 	});
 });
